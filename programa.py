ea55544981c3f34ed16eef6b5e11bb1c1834a981
WesleyAnibal <wesley.anibal@ccc.ufcg.edu.br>
2017-05-23 21:02:53 -0300

    função de minimo implementada

--- programa.py
+++ programa.py
@@ -5,3 +5,12 @@ def maximo(array):
             maior = i
 
     return maior
+
+def minimo(array):
+    menor = array[0]
+    for i in array:
+        if i < menor:
+            menor = i
+
+    return menor
+
